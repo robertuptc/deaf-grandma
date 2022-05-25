3d7f0c97ad76fa384adab4102f0e7a41109ab6ca
robertuptc <robertuptc@gmail.com>
2022-05-25 19:04:48 -0400

    finished exercise

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,29 +1,56 @@
-function deafGrandma() {
-    let goodByeCount = 0
+// function deafGrandma() {
+//     let goodByeCount = 0
     
 
-    grandma(prompt('HEY KID!'))
-    function grandma(firstPrompt) {
-        let heyKid = firstPrompt
-        if (heyKid === '') {
-            grandma(prompt('WHAT?!'))
-        } else if (heyKid === 'GOODBYE!') {
-            if (goodByeCount === 1) {
-                return
-            } else {
-                goodByeCount += 1
-                grandma(prompt('LEAVING SO SOON?'))
-            }
+//     grandma(prompt('HEY KID!'))
+//     function grandma(firstPrompt) {
+//         let heyKid = firstPrompt
+//         if (heyKid === '') {
+//             grandma(prompt('WHAT?!'))
+//         } else if (heyKid === 'GOODBYE!') {
+//             if (goodByeCount === 1) {
+//                 return
+//             } else {
+//                 goodByeCount += 1
+//                 grandma(prompt('LEAVING SO SOON?'))
+//             }
     
-        } else if (heyKid.split('').map(elem => elem.replace(/[^a-zA-Z]/, '')).join('').split('').every(elem => elem.match(/[A-Z]/))) {
-            grandma(prompt('NO,NOT SINCE 1946!'))
-        } else {
-            grandma(prompt('SPEAK UP, KID!'))
+//         } else if (heyKid.split('').map(elem => elem.replace(/[^a-zA-Z]/, '')).join('').split('').every(elem => elem.match(/[A-Z]/))) {
+//             grandma(prompt('NO,NOT SINCE 1946!'))
+//         } else {
+//             grandma(prompt('SPEAK UP, KID!'))
+//         }
+//     }
+//    return alert('LATER, SKATER!')
+// }
+
+
+
+// console.log(deafGrandma())
+
+function deafGrandma() {
+    counter=0
+    let text=window.prompt(“HEY KID!“)
+    while (counter!==2) {
+        if (text===“”) {
+            text=window.prompt(“WHAT?!“)
+        }
+        else if (text===“GOODBYE!“) {
+            if (counter===0) {
+            text=window.prompt(“LEAVING SO SOON?“)
+            counter+=1
+            }
+            else if (counter===1) {
+                counter+=1
+            alert(“LATER, SKATER!“)
+            }
+        }
+        else if (text!==text.toUpperCase()) {
+            text=window.prompt(“SPEAK UP, KID!“)
+        }
+        else if (text===text.toUpperCase()) {
+            text=window.prompt(“NO, NOT SINCE 1946!“)
         }
     }
-   return alert('LATER, SKATER!')
 }
-
-
-
-console.log(deafGrandma())
+deafGrandma()
--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,18 @@
 def deaf_grandma():
-
-
-
+    counter= 0
+    text = input("HEY KID!")
+    while counter != 2:
+        if text == "":
+            text = input("WHAT!")
+        elif text == "GOODBYE!":
+            if counter == 0:
+                counter += 1
+                text = input("LEAVING SO SOON?")
+            elif counter == 1:
+                counter += 1
+                print("LATER, SKATER!")
+        elif text.isupper():
+            text = input("NO, NOT SINCE 1946!")
+        else:
+            text = input("SPEAK UP, KID!")
 deaf_grandma()
